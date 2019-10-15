5881c1cd7151e83386e7ccfdd1f04c5b9ec8b7fe
username <shura_88_88@mail.ru>
2019-10-15 08:58:18 +0300

    update

--- src/library/BookAuthor.java
+++ src/library/BookAuthor.java
@@ -5,16 +5,22 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 
-public interface BookAuthor {
+public class BookAuthor {
 
     static void inpatAuthor(Connect connection) throws SQLException {
-        String str = "insert into Author (id, name, birstDate) values (?,?,?)";
+        String str = "insert into Author ( name, birstDate) values (?,?,?)";
         PreparedStatement pst = connection.getConnection().prepareStatement(str);
-        pst.setInt(1, 6);
         pst.setString(2, "Пушкин Александр Сергеевич");
         pst.setDate(3, Date.valueOf("1799-05-26"));
         pst.executeUpdate();
         System.out.println("Автор  добавлен");
+    }
+
+    static void deleteAuthor(Connect connection) throws SQLException {
+        String str = "delete from author where id=5 ";
+        PreparedStatement pst1 = connection.getConnection().prepareStatement(str);
+        pst1.executeUpdate();
+        System.out.println("Строки удалены");
     }
 
     static void inpatBook(Connect connection) throws SQLException {
@@ -29,10 +35,17 @@ public interface BookAuthor {
         System.out.println("Строки добавлены");
     }
 
-    static void delete(Connect connection) throws SQLException {
-        String str2 = "delete from author where id=5";
-        PreparedStatement pst1 = connection.getConnection().prepareStatement(str2);
-        pst1.executeUpdate();
+    static void deleteBook(Connect connection) throws SQLException {
+        String str2 = "delete from book where id=6";
+        PreparedStatement pst2 = connection.getConnection().prepareStatement(str2);
+        pst2.executeUpdate();
+        System.out.println("Строки удалены");
+    }
+
+    static void deleteGenre(Connect connection) throws SQLException {
+        String str3 = "delete from genre where id=1";
+        PreparedStatement pst3 = connection.getConnection().prepareStatement(str3);
+        pst3.executeUpdate();
         System.out.println("Строки удалены");
     }
 
